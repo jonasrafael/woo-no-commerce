6b5e2f30d3102420daba0534e564fb4b4927a54d
Jonas Rafael <iam@jonasrafael.com>
2014-07-24 21:30:31 -0300

    Create custom.js

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+// remove selected WooCommerce Products Tab
+//jQuery('.general_tab').remove();  // General
+jQuery('#general_product_data').css('display', 'none');  // hide General tab content as it is visible by default
+jQuery('._sale_price_field').css('display', 'none'); // remove promo-price field
+jQuery('._regular_price_field').css('display', 'none'); // remove regular-price field
+jQuery('.hndle').css('display', 'none');
+jQuery('.inventory_tab').remove();  // Inventory
+//jQuery('.shipping_tab').remove();  // Shipping
+//jQuery('.linked_product_tab').remove(); // Linked Products
+//jQuery('.attribute_tab').remove();  // Attributes
+//jQuery('.variations_tab').remove();  // Variations
+jQuery('.advanced_tab').remove();  // Advanced
